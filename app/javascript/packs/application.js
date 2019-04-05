92a5b24a2f0e375c2ef53b5a8dabc75c201cedbd
Dania Aflak <dania.aflak@skema.edu>
2019-04-05 20:19:36 +0200

    skills and first page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+
+import { hideCodingTools } from '../components/hide_skills_tools';
+import { hideOtherTools } from '../components/hide_skills_tools';
+
+hideCodingTools();
+hideOtherTools();
