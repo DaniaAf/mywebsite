0dbff2840019a19b062a5acf2f1fe634e159e4dd
Dania Aflak <dania.aflak@skema.edu>
2019-04-30 17:35:57 +0200

    sendgrid unsername and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,8 @@ Rails.application.configure do
 
     config.active_record.dump_schema_after_migration = false
 
+
+  # config for Contact Us form with Sendgrid - ActionMailer
   config.action_mailer.default_url_options = { host: 'https://dania-aflak.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
 
