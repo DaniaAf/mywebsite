82e61822600ccffe0d402ccb204c92224119465f
Dania Aflak <dania.aflak@skema.edu>
2019-05-02 17:23:20 +0200

    testing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'pages#home'
-  resources :contacts, only: [:new, :create]
+  resources :contacts, only: :new
+  resources :contacts, only: :create, to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
