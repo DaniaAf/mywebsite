00269e639fb3678335c4e0ab9157ae2a28c2a6b2
Dania Aflak <dania.aflak@skema.edu>
2019-05-02 17:05:07 +0200

    method new and create in page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,20 @@ class PagesController < ApplicationController
     @educations = Education.all
     @contact = Contact.new
 
-    # def new
-    #   @contact = Contact.new
-    # end
+    def new
+      @contact = Contact.new
+    end
 
-    # def create
-    #   @contact = Contact.new(params[:contact])
-    #   @contact.request = request
-    #   if @contact.deliver
-    #     flash.now[:error] = nil
-    #     flash.now[:notice] = 'Thank you for your message!'
-    #   else
-    #     flash.now[:error] = "Cannot send message."
-    #   end
-    # end
+    def create
+      @contact = Contact.new(params[:contact])
+      @contact.request = request
+      if @contact.deliver
+        flash.now[:error] = nil
+        flash.now[:notice] = 'Thank you for your message!'
+      else
+        flash.now[:error] = "Cannot send message."
+      end
+    end
 
   end
 
