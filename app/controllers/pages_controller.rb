1ff4247f1d854d663f87d42ca701fd322b0d6374
Dania Aflak <dania.aflak@skema.edu>
2019-05-02 17:14:14 +0200

    testing contact form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,19 +5,4 @@ class PagesController < ApplicationController
     @contact = Contact.new
   end
 
-  def new
-    @contact = Contact.new
-  end
-
-  def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:error] = nil
-      flash.now[:notice] = 'Thank you for your message!'
-    else
-      flash.now[:error] = "Cannot send message."
-    end
-  end
-
 end
