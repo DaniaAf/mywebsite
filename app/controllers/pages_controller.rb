82f28006f4b014dee9f6dc3d2b1bc1b417674435
Dania Aflak <dania.aflak@skema.edu>
2019-05-02 17:09:43 +0200

    testing new and create metho in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,22 +3,21 @@ class PagesController < ApplicationController
     @experiences = Experience.all
     @educations = Education.all
     @contact = Contact.new
+  end
 
-    def new
-      @contact = Contact.new
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+      flash.now[:notice] = 'Thank you for your message!'
+    else
+      flash.now[:error] = "Cannot send message."
     end
-
-    def create
-      @contact = Contact.new(params[:contact])
-      @contact.request = request
-      if @contact.deliver
-        flash.now[:error] = nil
-        flash.now[:notice] = 'Thank you for your message!'
-      else
-        flash.now[:error] = "Cannot send message."
-      end
-    end
-
   end
 
 end
