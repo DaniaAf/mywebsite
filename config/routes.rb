0f6220a516ea792a49d4a5c228638910b6bc3e36
Dania Aflak <dania.aflak@skema.edu>
2019-05-02 16:57:28 +0200

    contact form in only home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :contacts, only: [:new, :create]
+
   root to: 'pages#home'
+  resources :contacts, only: [:new, :create], to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
