d7ed6d392564f70fca139bbec71eecec837e14d1
Dania Aflak <dania.aflak@skema.edu>
2019-05-28 15:17:45 +0200

    testing contact form

--- app/javascript/components/validation_contact_form.js
+++ app/javascript/components/validation_contact_form.js
@@ -1,18 +1,18 @@
-const validationContactForm = () => {
-  window.addEventListener('load',function() {
-    var forms = document.getElementsByClassName('needs-validation');
-    var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('click', function(event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
+// const validationContactForm = () => {
+//   window.addEventListener('load',function() {
+//     var forms = document.getElementsByClassName('needs-validation');
+//     var validation = Array.prototype.filter.call(forms, function(form) {
+//       form.addEventListener('click', function(event) {
+//         if (form.checkValidity() === false) {
+//           event.preventDefault();
+//           event.stopPropagation();
+//         }
+//         form.classList.add('was-validated');
+//       }, false);
 
-    })
-  }, false)
+//     })
+//   }, false)
 
-}
+// }
 
-export { validationContactForm };
+// export { validationContactForm };
